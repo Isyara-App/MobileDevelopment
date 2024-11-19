aff8411f463ac03ee46ac97b6d70e6bd323c82c3
ifqygazhar <ifqygazhar@gmail.com>
2024-11-19 14:33:34 +0700

    Add config for url and api service for signup and login

---
+++ app/src/main/java/com/example/isyara/data/remote/retrofit/ApiConfig.kt
@@ -0,0 +1,31 @@
+package com.example.isyara.data.remote.retrofit
+
+import androidx.multidex.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ApiConfig {
+    companion object {
+        fun getApiService(): ApiService {
+            val clientBuilder = OkHttpClient.Builder()
+
+            if (BuildConfig.DEBUG) {
+                val loggingInterceptor =
+                    HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+                clientBuilder.addInterceptor(loggingInterceptor)
+            }
+
+            val client = clientBuilder.build()
+
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://f5be-114-124-147-195.ngrok-free.app/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+
+            return retrofit.create(ApiService::class.java)
+        }
+    }
+}
